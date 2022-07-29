b14b28914c6de3524e27bd194dc78f148ab775cc
HeseltineBTutu <heseltinetutu@gmail.com>
2022-07-29 09:24:45 +0300

    update and delete controllers added

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, jsonify, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+import sys
+from flask import abort
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__='todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+
+    def _repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+      description = request.get_json()['description']
+      todo = Todo(description=description)
+      db.session.add(todo)
+      db.session.commit()
+      body['description'] = todo.description
+
+    except:
+      error = True
+      db.session.rollback
+      print(sys.exc_info())
+    finally:
+      db.session.close()
+    if error== True:
+        abort(400)
+    else:
+        return jsonify(body)
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({'sucess':True})
+
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.order_by('id').all())
+
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
